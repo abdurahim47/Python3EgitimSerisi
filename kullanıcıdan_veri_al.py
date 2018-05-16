3baefbae8ba0fc16b436d2534d38b8735b3488a0
Abdurrahim <abdurahim.abdo.zer@gmail.com>
2018-05-16 11:52:25 +0300

    Update kullanıcıdan_veri_al.py

--- kullanıcıdan_veri_al.py
+++ kullanıcıdan_veri_al.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
+def get_address(metin="Adresi Giriniz: "):
+    """Kullanıcıdan alınan adres verisinin \n gibi şeylere maruz kalmaması için yazıldı"""
+    adres = input(metin)
+    return r"".join(adres)
+
 def get_char(metin="Lütfen Bir Karakter Giriniz: "):
+    """Kullanıcıdan tek bir karakter almak için tasarlandı."""
     kabul = "abcçdefgğhıijklmnoöprsştuüvyzqwxABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZQWX"
     girdi_char = str(input(metin))
     while len(girdi_char) != 1:
@@ -11,6 +17,7 @@ def get_char(metin="Lütfen Bir Karakter Giriniz: "):
     return girdi_char
 
 def get_string(metin="Lütfen Bir String Giriniz: "):
+    """Kullanıcıdan bir karakter katarı almak için tasarlandı."""
     while True:
         try:
             girdi_string = str(input(metin))
@@ -21,6 +28,7 @@ def get_string(metin="Lütfen Bir String Giriniz: "):
             return girdi_string
 
 def get_int(metin="Lütfen Bir Tamsayı Giriniz: "):
+    """Kullanıcıdan tamsayı alımı için tasarlandı."""
     while True:
         try:
             girdi_int = int(input(metin))
@@ -31,6 +39,7 @@ def get_int(metin="Lütfen Bir Tamsayı Giriniz: "):
             return girdi_int
 
 def get_pos_int(metin="Lütfen Pozitif Bir Tamsayı Giriniz: "):
+    """Kullanıcıdan pozitif tamsayı alımı için tasarlandı."""
     sayı = get_int(metin)
     while sayı <= 0:
         sayı = get_pos_int(metin)
@@ -38,6 +47,7 @@ def get_pos_int(metin="Lütfen Pozitif Bir Tamsayı Giriniz: "):
         return sayı
 
 def get_neg_int(metin="Lütfen Negatif Bir Tamsayı Giriniz: "):
+    """Kullanıcıdan negatif tamsayı alımı için tasarlandı."""
     sayı = get_int(metin)
     while sayı >= 0:
         sayı = get_neg_int(metin)
@@ -45,6 +55,7 @@ def get_neg_int(metin="Lütfen Negatif Bir Tamsayı Giriniz: "):
         return sayı
 
 def get_float(metin="Lütfen Bir Ondalıklı Sayı Giriniz: "):
+    """Kullanıcıdan ondalıklı sayı alımı için tasarlandı."""
     while True:
         try:
             metin = str(metin)
@@ -56,6 +67,7 @@ def get_float(metin="Lütfen Bir Ondalıklı Sayı Giriniz: "):
             return girdi_float
 
 def get_pos_float(metin="Lütfen Pozitif Bir Ondalıklı Sayı Giriniz: "):
+    """Kullanıcıdan pozitif ondalıklı sayı alımı için tasarlandı."""
     sayı = get_float(metin)
     while sayı <= 0:
         sayı = get_pos_float(metin)
@@ -63,6 +75,7 @@ def get_pos_float(metin="Lütfen Pozitif Bir Ondalıklı Sayı Giriniz: "):
         return sayı
 
 def get_neg_float(metin="Lütfen Negatif Bir Ondalıklı Sayı Giriniz: "):
+    """Kullanıcıdan negatif ondalıklı sayı alımı için tasarlandı."""
     sayı = get_float(metin)
     while sayı >= 0:
         sayı = get_neg_float(metin)
@@ -70,6 +83,7 @@ def get_neg_float(metin="Lütfen Negatif Bir Ondalıklı Sayı Giriniz: "):
         return sayı
 
 def get_zero(metin="Lütfen Sıfır Değerini Giriniz: "):
+    """Kullanıcıdan sıfır sayısının alımı için tasarlandı."""
     sayı = get_float(metin)
     while sayı != 0.0:
         sayı = get_zero(metin)
